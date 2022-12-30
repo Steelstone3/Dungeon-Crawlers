81716da2c9899ac20c10d1cce74e4e6c1d08e39e
Skippy <steelstone3@gmail.com>
2022-12-30 03:37:24 +0000

    Update CharacterCreationSystem.cs

--- DungeonCrawlers/Systems/CharacterCreationSystem.cs
+++ DungeonCrawlers/Systems/CharacterCreationSystem.cs
@@ -24,6 +24,12 @@ namespace DungeonCrawlersTests.Systems
 
             for (int i = 0; i < quantity; i++)
             {
+                // TODO AH Command to spawn a random character.
+
+                // Could be done from a predefined list of characters which is shuffled 
+                // with the selection being removed from the list each time
+
+                // Could also be done with a list of random names and races similarly
                 characterParty.Add(gamePresenter.CreateCharacter());
             }
 
