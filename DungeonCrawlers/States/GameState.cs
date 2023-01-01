cfc33fe713be95e507971860d3b3454a0b5bc60d
Skippy <steelstone3@gmail.com>
2023-01-01 02:34:19 +0000

    Update GameState.cs

--- DungeonCrawlers/States/GameState.cs
+++ DungeonCrawlers/States/GameState.cs
@@ -2,11 +2,19 @@ namespace DungeonCrawlers.States
 {
     public abstract class GameState : IGameState
     {
+        private readonly IGameStateRepository gameStateRepository;
+
+        public GameState(IGameStateRepository gameStateRepository)
+        {
+            this.gameStateRepository = gameStateRepository;
+        }
+
         public abstract void StartState();
 
-        public void GoToState()
+        public void GoToState(IGameState gameState)
         {
-            
+            gameStateRepository.GameState = gameState;
+            gameStateRepository.GameState.StartState();
         }
     }
 }
