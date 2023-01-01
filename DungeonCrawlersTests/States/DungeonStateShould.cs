89c1f6edbaf07f05b80a1d741691e02ddfec1799
Skippy <steelstone3@gmail.com>
2023-01-01 12:11:23 +0000

    Tested spawn monsters

--- DungeonCrawlersTests/States/DungeonStateShould.cs
+++ DungeonCrawlersTests/States/DungeonStateShould.cs
@@ -1,7 +1,63 @@
+using System.Collections.Generic;
+using DungeonCrawlers.Entities;
+using DungeonCrawlers.Presenters;
+using DungeonCrawlers.States;
+using DungeonCrawlers.Systems;
+using Moq;
+using Xunit;
+
 namespace DungeonCrawlersTests.States
 {
     public class DungeonStateShould
     {
+        Mock<IGameStateRepository> gameStateRepository = new();
+        Mock<IPresenter> presenter = new();
+        Mock<IGameRepository> gameRepository = new();
+        Mock<IMonsterCreationSystem> monsterCreationSystem = new();
+        Mock<ICombatSystem> combatSystem = new();
+        Mock<ISeededRandomSystem> seededRandomSystem = new();
+        IGameState gameState;
 
+        public DungeonStateShould()
+        {
+            gameRepository.Setup(gr => gr.CharacterParty).Returns(new List<ICharacter>());
+            gameRepository.Setup(gr => gr.MonsterParty).Returns(new List<IMonster>());
+            gameStateRepository.Setup(gsr => gsr.GameState).Returns(gameState);
+            gameStateRepository.Setup(gsr => gsr.GameState.StartState());
+            gameState = new DungeonState(gameStateRepository.Object, presenter.Object, gameRepository.Object, monsterCreationSystem.Object, combatSystem.Object, seededRandomSystem.Object);
+        }
+
+        [Fact]
+        public void SpawnMonsters()
+        {
+            // Given
+            var quantity = 5;
+            var seeds = new int[] { 1, 1, 1, 1, 1 };
+            var monster = new Monster(null, null, null, null);
+            var monsters = new IMonster[] { monster, monster, monster, monster, monster };
+            seededRandomSystem.Setup(srs => srs.GetRandom(1, 10)).Returns((ulong)quantity);
+            seededRandomSystem.Setup(srs => srs.CreateSeeds(quantity)).Returns(seeds);
+            monsterCreationSystem.Setup(srs => srs.CreateMultiple(quantity, seeds)).Returns(monsters);
+            presenter.Setup(p => p.PrintParty(monsters));
+
+            // When
+            gameState.StartState();
+
+            // Then
+            seededRandomSystem.VerifyAll();
+            monsterCreationSystem.VerifyAll();
+            presenter.VerifyAll();
+        }
+
+        [Fact(Skip="Later")]
+        public void StartCombat()
+        {
+            // Given
+
+            // When
+            gameState.StartState();
+
+            // Then
+        }
     }
 }
