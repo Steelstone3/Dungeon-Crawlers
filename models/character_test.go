5f1cf6e73488d02f38b5c70f31d185d4db9c3102
Skippy <steelstone3@gmail.com>
2025-01-16 03:35:04 +0000

    version-16-01-25

---
+++ models/character_test.go
@@ -0,0 +1,34 @@
+package models
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func CreateCharacter(t *testing.T) {
+	// Given
+	const name = "Jeff"
+	const class = "Mage"
+	const maximumDamage = 10
+	const minimumDamage = 5
+	const maximumArmour = 100
+	const armour = 100
+	expectedCharacter := Character{
+		Name:  name,
+		Class: class,
+		Weapon: Weapon{
+			MaximumDamage: maximumDamage,
+			MinimumDamage: minimumDamage,
+		},
+		Armour: Armour{
+			MaximumArmour: maximumArmour,
+			Armour:        armour,
+		},
+	}
+
+	// When
+	actualCharacter := createCharacter(name,class)
+
+	// Then
+	assert.Equal(t, expectedCharacter, actualCharacter)
+}
